2f33b17e1d7f56cc5dc0db3f8c3352b442a8d85a
Kauan Linhares <kauan-linhares@hotmail.com>
2021-05-01 20:56:55 -0300

    Revisão final da classe Principal

--- QuadrosKP/src/Principal.java
+++ QuadrosKP/src/Principal.java
@@ -3,14 +3,16 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Retangulo quadroRetangulo1 = new Retangulo(0.1, 0.1);
-		System.out.printf("Área do quadro: %.2f\n", quadroRetangulo1.area);
+		System.out.printf("--- Quadro Retangular ---\n");
+		System.out.printf("Area do quadro: %.2f\n", quadroRetangulo1.area);
 		System.out.printf("Perimetro do quadro: %.2f\n", quadroRetangulo1.perimetro);
-
+		System.out.printf("Valor do servico: R$%.2f\n\n", quadroRetangulo1.calculaValorDoServico());
 
 		Circunferencia quadroCircunferencia1 = new Circunferencia(0.1);
-		System.out.printf("Área do quadro: %.2f\n", quadroCircunferencia1.area);
+		System.out.printf("---- Quadro Circular ----\n");
+		System.out.printf("Area do quadro: %.2f\n", quadroCircunferencia1.area);
 		System.out.printf("Perimetro do quadro: %.2f\n", quadroCircunferencia1.perimetro);
-		System.out.printf("Valor do serviço: R$%.2f\n", quadroCircunferencia1.calculaValorDoServico());
+		System.out.printf("Valor do servico: R$%.2f\n", quadroCircunferencia1.calculaValorDoServico());
 	}
 
 }
