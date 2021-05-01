65711fd6df7a3f39ec7c177b40c5316ffd8007a2
Pedro Freitas <koii.tkd@gmail.com>
2021-05-01 20:22:30 -0300

    Desenvolvimento das classes Quadro e Circunferencia

--- QuadrosKP/src/Circunferencia.java
+++ QuadrosKP/src/Circunferencia.java
@@ -1,4 +1,19 @@
+import java.math.*;
 
 public class Circunferencia extends Quadro {
-	
+	public double raio;
+
+    public Circunferencia(double raio) {
+        this.raio = raio;
+        area();
+        perimetro();
+    }
+
+    public void area(){
+        this.area = Math.PI * Math.pow(raio, 2);
+    }
+
+    public void perimetro(){
+        this.perimetro = 2 * Math.PI * raio;
+    }
 }
--- QuadrosKP/src/Quadro.java
+++ QuadrosKP/src/Quadro.java
@@ -1,4 +1,15 @@
 
 public class Quadro {
+    
+    public static double valorPapel = 10;
+    public static double valorMoldura = 20;
+    public static double maoDeObra = 0.05;
+
+    public double area;
+    public double perimetro;
+
+    public double calculaValorDoServico(){
+        return (area*valorPapel+perimetro*valorMoldura)*(1+maoDeObra);
+    }
 
 }
