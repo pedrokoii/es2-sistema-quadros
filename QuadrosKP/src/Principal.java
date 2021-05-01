f2abfa02f2198f71ebb7ecf08922b2484eb0784f
Kauan Linhares <kauan-linhares@hotmail.com>
2021-05-01 19:15:44 -0300

    Classe Retangulo modificada

--- QuadrosKP/src/Principal.java
+++ QuadrosKP/src/Principal.java
@@ -1,4 +1,4 @@
-
+  
 public class Principal {
 
 	public static void main(String[] args) {
--- QuadrosKP/src/Retangulo.java
+++ QuadrosKP/src/Retangulo.java
@@ -1,4 +1,25 @@
+import javax.swing.JOptionPane;
 
 public class Retangulo extends Quadro {
+	
+	public Retangulo(double area, double perimetro) {
+		super(area, perimetro);
+		// TODO Auto-generated constructor stub
+	}
 
+	public double base, altura;
+	
+	void Calculo() {
+		String aux = JOptionPane.showInputDialog(null, "Digite a base");
+		base = Double.parseDouble(aux);
+		
+		aux = JOptionPane.showInputDialog(null, "Digite a altura");
+		altura = Double.parseDouble(aux);
+		
+		area = base * altura;
+		perimetro = 2 * (base + altura);
+		
+		JOptionPane.showMessageDialog(null, "área = " + area);
+		JOptionPane.showMessageDialog(null, "Perímetro = " + perimetro);
+	}
 }
